ce9f0d40b25e72b22d1b122334c5ff3b5f0c2a4f
Saipranav <saipranav.kotamreddy@gmail.com>
2022-04-16 16:35:32 -0700

    About Page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,7 +3,23 @@ import React from 'react';
 const About = () => {
   return (
     <div className="About">
-      About Page
+      <h4>Hello welcome to Bite Share, the wonderful website designed to help lower wasted food in the Davis area. This site is designed
+      to combat the large number of food disposed of each day despite being perfectly edible. Hunger is a problem faced by several 
+      counties in California, but this website would like to combat it in Davis specifically.
+      
+       After registering an account, you have the option of either posting extra food you have, searching for food if you would like some
+      but are unable to, or just want to see what people are leaving behind. This site is not designed just for individuals, as food banks 
+      and the like are also hosted and noted for those who would like that. 
+      
+      After selecting what food you would like to pick up, a map will pop up showing you where to go to receive thefood, alongside a text to 
+      both the person picking it up and the person offering, allowing for both parties to be informed of the coming interaction.
+
+      We hope that this mitigates at least some of the food surplus and hunger across Davis and that social welfare is improved.
+      Thanks for reading!
+      </h4>
+      <h15>(Disclaimer: users posting food will be required to list their address as to note where to pick up food from. If this is an issue, please
+      register your account using address you can drop the food off at, but wpuld feel comfortable meeting others at. Only registered users are able
+      to see these addresses, so security is fairly well-kept)</h15>
     </div>
   );
 }
