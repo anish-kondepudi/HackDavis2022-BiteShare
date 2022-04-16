8debefc67976b66772a5cf007867b21882cd9327
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-04-16 14:11:01 -0700

    Added temp dispatch for redux ui testing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { setEmail } from '../actions';
+
+
 const Home = () => {
+
+  const userEmail = useSelector(state => state.email);
+  const dispatch = useDispatch();
+  
   return (
     <div className="Home">
       Home Page
+      <button onClick={() => {dispatch(setEmail("anish@gmail.com"))}}>login</button>
+      <button onClick={() => {dispatch(setEmail(""))}}>logout</button>
     </div>
   );
 }
