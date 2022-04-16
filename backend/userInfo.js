acd28af47fead1db29f55c282b0505dd3fc2e931
Saipranav <saipranav.kotamreddy@gmail.com>
2022-04-16 13:48:11 -0700

    User post added

--- backend/userInfo.js
+++ backend/userInfo.js
@@ -1,30 +1,22 @@
 const express = require('express');
 const router=express.Router();
+const { ObjectId } = require("mongodb");
 const app = express();
 const port= process.env.PORT || 5000;
 const cors = require('cors');
 const mongoose = require('mongoose');
 
 app.use(cors());
-app.use(mongoose());
 app.use(express.json());
-app.use('/', router);
+//app.use('/', router);
 const url = "mongodb+srv://dbUser:IIpKZGMaDoVjPt9e@cluster0.lq8ew.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
-mongoose.connect(url).then(
-    () => {
-        console.log("Connected");
-    },
-    err => {
-        console.log("Error" + err);
-    }
-);
 let database = mongoose.connection;
 let userSchema= new mongoose.Schema({
     username: String,
     email: String,
     password: String,
-    Address: String,
+    address: String,
     phoneNumber: String
 });
 
@@ -46,9 +38,34 @@ app.get('/', function(req,res,next){
 >>>>>>> b37ca6d (api user get)
 =======
 let userModel=mongoose.model('users',userSchema);
-module.exports=mongoose.model("Users", userModel);
-app.get('/', function(req,res,next){
-    console.log("Connected");
+module.exports=function(app){
+    console.log("hi");
+    app.get("/users", (req, res) => {
+        userModel.find().then(function(err, foundUser){
+            console.log(foundUser);
+            res.send(foundUser);
+        });
+    });
+    app.post("/addUser", (req, res) => {
+        let newUser = req.body;
+        userModel.collection.insertOne(newUser, function(err, docs){
+            if (err) {
+                res.send({"Error": err});
+            } else {
+                res.send({"Success": true});
+            }
+        })
+    });
+}
+/*app.get("/users", (req, res) => {
+    console.log("hi");
+    const data = userModel.find();
+    console.log(data);
+    res.send(data);
 });
+<<<<<<< HEAD
 >>>>>>> e9949c6 (temp2)
 app.listen(PORT, () => `Server running on port ${PORT}`);
+=======
+*/
+>>>>>>> fd67a27 (User post added)
