f09e0e0b94573b3dc7414316646ae0a442e001f3
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-04-17 06:53:12 -0700

    Removed duplicate files

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -46,7 +46,7 @@ const Login = () => {
  
   return (
     <div className="Login">
-      <Form>
+     <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange}/>
