a21d707d5bdd446bbcbe76dc277e85f078511c6f
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-04-16 17:59:30 -0700

    Map Page progress

--- client/src/pages/PickupMap.js
+++ client/src/pages/PickupMap.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-const PickupMap = () => {
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+const MapContainer = (props) => {
   return (
-    <div className="PickupMap">
-      PickupMap Page
-    </div>
+    <Map
+      google={props.google}
+      zoom={14}
+      style={mapStyles}
+      initialCenter={
+        {
+          lat: -1.2884,
+          lng: 36.8233
+        }
+      }
+    />
   );
 }
 
-export default PickupMap;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBTVIJwaV2ppI-Cb6H8NMfG3ALh0NztJfQ'
+})(MapContainer);
