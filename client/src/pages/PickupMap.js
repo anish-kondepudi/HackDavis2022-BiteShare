6816efb2d94966f94a3c369aef24acc91564af6c
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-04-16 23:06:04 -0700

    Fetch marker data

--- client/src/pages/PickupMap.js
+++ client/src/pages/PickupMap.js
@@ -5,6 +5,13 @@ import {
   marker,
   InfoWindow,
 } from '@react-google-maps/api';
+import Geocode from "react-geocode";
+
+Geocode.setApiKey("AIzaSyBTVIJwaV2ppI-Cb6H8NMfG3ALh0NztJfQ");
+Geocode.setLanguage("en");
+Geocode.setLocationType("ROOFTOP");
+Geocode.enableDebug();
+
 
 
 const PickupMap = (props) => {
@@ -30,18 +37,68 @@ const PickupMap = (props) => {
     }
 
     // Grab Location Markers
-    console.log("hiii");
+    
     let url = 'http://localhost:5000/food/';
     fetch(url).then(res => {
       if(!res.ok){
-        console.log(res);
-        // throw res;
+        throw res;
       }
       return res.json();
     }).then(data => {
-      console.log(data);
-    });
+      const emailMap = new Map();
+      for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            emailMap.set(new Promise((resolve) => {
+              const id = data[key]._id;
+              let url = 'http://localhost:5000/users/' + data[key].email;
+              fetch(url).then(res => {
+                if(!res.ok){
+                  throw res;
+                }
+                return res.json();
+              }).then(data => {
+                data.forEach((record, _) => {
+                  resolve({
+                    "address": record.address,
+                    "id": id
+                  });
+                })
+              });
+            }));
+            return emailMap;
+        }
+      }
+      //return emailMap
+    }).then(emailMap => {
+      const listOfFoods = [];
+      Promise.all(emailMap).then(res => {
+        for(let i = 0; i < res.length; i++) {
+          res[i][0].then(value => {
+            Geocode.fromAddress(value.address).then(
+              (response) => {
+                const { lat, lng } = response.results[0].geometry.location;
+                const foodData = {"lat": lat, "lng": lng, "address": value.address, "id": value.id}
+                setMarkers((current) => [
+                  ...current,
+                  foodData,
+                ]);
+                console.log("lol");
+                console.log(markers);
+                console.log("lel");
+              },
+              (error) => {
+                console.error(error);
+              }
+            );
+          })
+        }
 
+
+        // console.log("listofFoodes: ");
+        // console.log(listOfFoods[0]);
+        // console.log("done");
+      });
+    });
   }, []);
 
   const libraries = ['places'];
@@ -70,6 +127,10 @@ const PickupMap = (props) => {
   if (!isLoaded) return "Loading Maps";
 
   return (
+    <>
+    <button onClick={()=> {
+      console.log(markers);
+    }}>thic boi</button>
     <div className='PickupMap'>
       <GoogleMap 
         mapContainerStyle={mapContainerStyle}
@@ -79,6 +140,7 @@ const PickupMap = (props) => {
       >
       </GoogleMap>
     </div>
+    </>
   );
 };
 
