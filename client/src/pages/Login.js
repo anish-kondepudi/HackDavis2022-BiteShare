18b3c3bf2991f4d49f557087d82b6feeaa2e2952
Saipranav <saipranav.kotamreddy@gmail.com>
2022-04-16 18:39:29 -0700

    login page working

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,28 +1,68 @@
+ 
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './styles/Login.css';
-
-
-
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setEmail } from '../actions';
+import { useNavigate } from "react-router-dom";
+ 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+ 
+  const [email, setEmail2] = useState("");
+  const [password, setPassword] = useState("");
+ 
+  const handleEmailChange = (event) => {
+    setEmail2(event.target.value);
+  }
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+ 
+  const handleSubmit = (event) => {
+    const requestParams= {
+      method: 'GET',
+      headers:{
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+    };
+    let url = 'http://localhost:5000/users/' + email;
+    fetch(url).then(res => {
+      console.log(res);
+      if(!res.ok){
+        throw res;
+      }
+      console.log(res.body.password);
+      return res.json();
+    }).then(data => {
+      if(password===data[0]["password"]){
+        dispatch(setEmail(email));
+        navigate("/");
+      }
+      console.log("password data: " + data[0]["password"]);
+    });
+};
+ 
   return (
     <div className="Login">
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={handlePasswordChange}/>
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" onClick={handleSubmit}>
           Submit
         </Button>
       </Form>
     </div>
   );
 }
-
 export default Login;
