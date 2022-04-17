eacb7af05b105574a3a089f6a6219f9afa383078
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-04-16 18:03:56 -0700

    Showing user email on homepage for debugging

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,6 +12,7 @@ const Home = () => {
   return (
     <div className="Home">
       This is the homepage. Mock being logged in or logged out using buttons below.
+      <h1>Your email is: [{userEmail}]</h1>
       <br/>
       <button onClick={() => {dispatch(setEmail("anish@gmail.com"))}}>login</button>
       <button onClick={() => {dispatch(setEmail(""))}}>logout</button>
