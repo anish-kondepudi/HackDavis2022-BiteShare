810a215c8ae4a570f363ea07df422b22e26f4116
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-04-17 02:58:37 -0700

    Added popup twilio database mongo fetch request and post request

--- client/src/pages/PickupMap.js
+++ client/src/pages/PickupMap.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef, useCallback } from 'react';
+import { useSelector } from 'react-redux';
 import {
   GoogleMap,
   useLoadScript,
@@ -7,11 +8,22 @@ import {
 } from '@react-google-maps/api';
 import Geocode from "react-geocode";
 import './styles/PickupMap.css';
+import Popup from "./Popup";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-const PickupMap = (props) => {
+const PickupMap = () => {
 
   const [markers, setMarkers ] = useState([]);
   const [selected, setSelected] = useState(null);
+
+  const userEmail = useSelector(state => state.email);
+  const [showPopUp, setShowPopup] = useState(false);
+  const [popupCurrImgData, setPopupCurrImgData] = useState();
+  const [popupCurrName, setPopupCurrName] = useState();
+  const [popupCurrEmail, setPopupCurrEmail] = useState();
+  const [popupCurrId, setPopupCurrId] = useState();
+  const [popupCurrDesc, setPopupCurrDesc] = useState();
 
   // Hard Coding 750 Orchard Rd, Davis, CA 95616
   const [lat, setLat] = useState(38.543770);
@@ -49,6 +61,11 @@ const PickupMap = (props) => {
         if (data.hasOwnProperty(key)) {
             emailMap.set(new Promise((resolve) => {
               const id = data[key]._id;
+              const description = data[key].description;
+              const img = data[key].data;
+              const name = data[key].name;
+              const startTime = data[key].startTime;
+              const endTime = data[key].endTime;
               let url = 'http://localhost:5000/users/' + data[key].email;
               fetch(url).then(res => {
                 if(!res.ok){
@@ -60,7 +77,14 @@ const PickupMap = (props) => {
                   resolve({
                     "address": record.address,
                     "id": id,
-                    "name": record.username
+                    "username": record.username,
+                    "description": description,
+                    "img": img,
+                    "name": name,
+                    "startTime": startTime,
+                    "endTime": endTime,
+                    "number": record.phoneNumber,
+                    "email": record.email
                   });
                 })
               });
@@ -75,7 +99,20 @@ const PickupMap = (props) => {
             Geocode.fromAddress(value.address).then(
               (response) => {
                 const { lat, lng } = response.results[0].geometry.location;
-                const foodData = {"lat": lat, "lng": lng, "address": value.address, "id": value.id, "name": value.name}
+                const foodData = {
+                  "lat": lat, 
+                  "lng": lng, 
+                  "address": value.address, 
+                  "id": value.id,
+                  "username": value.username,
+                  "description": value.description,
+                  "img": value.img,
+                  "name": value.name,
+                  "startTime": value.startTime,
+                  "endTime": value.endTime,
+                  "number": value.phoneNumber,
+                  "email": value.email
+                }
                 setMarkers((current) => [
                   ...current,
                   foodData,
@@ -90,6 +127,62 @@ const PickupMap = (props) => {
       });
     });
   }, []);
+
+  const handlePopupClose = (event) => {
+    setShowPopup(false);
+  }
+
+  const getFoodClicked = () => {
+    console.log("Food id: " + popupCurrId)
+    // two emails -> food email, and user curr email
+    // need to get the phone number of both
+    console.log("User email: " + userEmail);
+    fetch("http://localhost:5000/users/" + userEmail).then(res => res.json()).then(currUserData => {
+      fetch("http://localhost:5000/users/" + popupCurrEmail).then(res => res.json()).then(popupUserData => {
+        let userPhoneNumber = currUserData[0]["phoneNumber"];
+        let donorPhoneNumber = popupUserData[0]["phoneNumber"];
+        let donorAddress = popupUserData[0]["address"];
+        console.log("User phone number: " + userPhoneNumber);
+        console.log("Donor phone number: " + donorPhoneNumber);
+        console.log("Donor address: " + donorAddress);
+        fetch("http://localhost:5000/pickup/" + popupCurrId, {
+          method: 'PUT', 
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+           },
+          body: JSON.stringify({donorPhoneNumber: donorPhoneNumber, userPhoneNumber: userPhoneNumber, donorAddress: donorAddress})}).then(res => res.json()).then(data => {
+            console.log(data);
+          })
+      })
+    })
+
+    /*
+    fetch("http://localhost:5000/pickup/" + popupCurrId, {
+        method: 'PUT', 
+        body: JSON.stringify({
+        donorPhoneNumber: 
+      })
+    }).then(res => res.json()).then(data => {
+      console.log("Data: " + data);
+    })*/
+  }
+
+  const cardClicked = (indFood) => {
+    console.log(indFood.img)
+    console.log(indFood.name);
+    console.log(indFood.email);
+    console.log(indFood.id);
+    console.log(indFood.description);
+
+
+    setPopupCurrImgData(indFood.img)
+    setPopupCurrName(indFood.name);
+    setPopupCurrEmail(indFood.email);
+    setPopupCurrId(indFood.id);
+    setPopupCurrDesc(indFood.description);
+    setShowPopup(true)
+  }
 
   const libraries = ['places'];
 
@@ -146,13 +239,32 @@ const PickupMap = (props) => {
         >
           <div>
             <h2>Food Popup</h2>
-            <p>{selected.name}</p>
-            <p>{selected.address}</p>
-            <p>{selected.id}</p>
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <img src={selected.img} id = "usersFood"></img>
+                <Card.Title>{selected.name}</Card.Title>
+                <Card.Text>
+                  Posted by: {selected.email}
+                </Card.Text>
+                <Card.Text>
+                  {selected.description}
+                </Card.Text>
+                <Button onClick={() => cardClicked(selected)}> I am interested </Button>
+              </Card.Body>
+            </Card>
           </div>
         </InfoWindow> : null}
       </GoogleMap>
     </div>
+    {showPopUp && <Popup content={
+        <>
+          <img src={popupCurrImgData} id="usersFood"></img>
+          <h1> {popupCurrName} </h1>
+          <p> Posted by: {popupCurrEmail} </p>
+          <p> {popupCurrDesc} </p>
+          <Button onClick={() => getFoodClicked()}> Reserve Food </Button>
+        </>
+        } handleClose={handlePopupClose} ></Popup>}
     </>
   );
 };
