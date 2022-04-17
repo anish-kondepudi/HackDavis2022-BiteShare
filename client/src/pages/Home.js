dd2b1411f3de063260cc772d35beac9f0ffb63a4
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-04-17 06:10:12 -0700

    Made Home Page Nice

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,59 @@
 import React from 'react';
-
-import { useSelector, useDispatch } from 'react-redux';
-import { setEmail } from '../actions';
-
+import { useSelector } from 'react-redux';
+import './styles/Home.scss';
+import banner from '../components/images/banner.png';
+import { useEffect , useState} from 'react';
 
 const Home = () => {
 
   const userEmail = useSelector(state => state.email);
-  const dispatch = useDispatch();
-  
+  const [username, setUserName] = useState("");
+  useEffect(() => {
+    if(userEmail) {
+      fetch("http://localhost:5000/users/" + userEmail).then(res => res.json()).then(data => {
+        setUserName(data[0]["username"]);
+      })
+    }
+  }, []);
+
+  const bodyText = "Welcome to Bite Share, the wonderful website designed to help lower wasted food in the Davis area. This site is designed to combat the large number of food disposed of each day despite being perfectly edible. Hunger is a problem faced by several counties in California, but this website would like to combat it in Davis specifically.";
+
   return (
     <div className="Home">
-      This is the homepage. Mock being logged in or logged out using buttons below.
-      <h1>Your email is: [{userEmail}]</h1>
-      <br/>
-      <button onClick={() => {dispatch(setEmail("anish@gmail.com"))}}>login</button>
-      <button onClick={() => {dispatch(setEmail(""))}}>logout</button>
+      <div className="container">
+        <div className="grid">
+          <div className="column-xs-12">
+            <ul className="slider">
+              <li className="slider-item active">
+                <div className="grid vertical">
+                  <div className="column-xs-12 column-md-2 hide-mobile">
+                    <div className="intro">
+                      <a href="#">
+                        <h1 className="title"><span className="underline">Explore Tokyo</span></h1>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="column-xs-12 column-md-10">
+                    <div className="image-holder">
+                      <img src={banner}/>
+                    </div>
+                    <div className="grid">
+                      <div className="column-xs-12 column-md-9">
+                        <div className="intro show-mobile">
+                          <a href="#">
+                            <h1 className="title"><span className="underline">Welcome{username ? " "+username : ''}!</span></h1>
+                          </a>
+                        </div>
+                        <p className="description">{bodyText}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                </li>
+            </ul>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
