2b906e11691ea4103528a0ed32f8bf797ace9da5
RachitSharma2001 <rachitsharma613@gmail.com>
2022-04-17 00:04:03 -0700

    Pick up list done

--- client/src/pages/PickupList.js
+++ client/src/pages/PickupList.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
 
 const PickupList = () => {
+  const [allFood, setAllFood] = useState([]);
+  const userEmail = useSelector(state => state.email);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/food').then(res => res.json()).then(data => {
+      let indFood = []
+      for(let key in data) {
+        indFood.push({imgData: data[key].data, name: data[key].name, desc: data[key].description, email: data[key].email});
+      }
+      setAllFood(indFood);
+    });
+
+    // }}).then(data => {
+    //   /*console.log("Data length after get: " + data.length);
+    //   let indFood = []
+    //   for(let i = 0; i < data.length; i++) {
+    //     indFood.push({imgData: data[i].data, name: data[i].name, desc: data[i].description, email: data[i].email});
+    //   }
+    //   setAllFood(indFood);*/
+    //   /*for(let key in data) {
+    //     console.log("Key: " + key);
+    //   }*/
+    // });
+  }, []);
+
   return (
     <div className="PickupList">
-      PickupList Page
+      <div className="DonateView">
+      {allFood.map((indFood) => 
+        <div class = "individualCard">
+        <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <img src={indFood.imgData} id = "usersFood"></img>
+          <Card.Title>{indFood.name}</Card.Title>
+          <Card.Text>
+            {indFood.desc}
+          </Card.Text>
+          <Card.Text>
+            {indFood.email}
+          </Card.Text>
+        </Card.Body>
+        </Card>
+        </div>
+      )}
+      
+    </div>
     </div>
   );
 }
