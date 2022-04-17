9b762fa968cbc7339d033ff8c16e1430a93bc616
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-04-16 19:14:58 -0700

    Added map and pulled user's current location

---
+++ client/src/components/Navigation 2.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import logo from './images/logo.png'
+import { useSelector } from 'react-redux';
+
+const Navigation = () => {
+
+  const userEmail = useSelector(state => state.email);
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <img src={logo} width="50" height="30" className="d-inline-block align-top" alt=""/>
+      <a className="navbar-brand" href="#">Navbar</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          {userEmail ? <>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Donate</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Pick Up</a>
+            </li></> : ''
+          }
+          <li className="nav-item">
+            <a className="nav-link" href="#">About</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="collapse navbar-collapse justify-content-end mr-5">
+        <ul className="navbar-nav mr-auto">
+          {userEmail ?<>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Logout</a>
+            </li></> :
+             <><li className="nav-item">
+             <a className="nav-link" href="#">Login</a>
+           </li>
+           <li className="nav-item">
+             <a className="nav-link" href="#">Register</a>
+           </li></>
+          }
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
--- client/src/pages/PickupMap.js
+++ client/src/pages/PickupMap.js
@@ -1,27 +1,65 @@
-import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import React, { useEffect, useState } from 'react';
+import {
+  GoogleMap,
+  useLoadScript,
+  marker,
+  InfoWindow,
+} from '@react-google-maps/api';
 
-const mapStyles = {
-  width: '100%',
-  height: '100%'
+
+const PickupMap = (props) => {
+
+  const [lat, setLat] = useState(37.430759);
+  const [lng, setLng] = useState(-121.897507);
+
+  console.log(lat, lng);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      console.error('Geolocation is not supported by your browser');
+    } else {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+      }, () => {
+        console.error('Unable to retrieve your location');
+      });
+    }
+  }, []);
+
+  const libraries = ['places'];
+  const mapContainerStyle = {
+    width: '100vw',
+    height: '100vh'
+  }
+  const center = {
+    lat: lat,
+    lng: lng
+  }
+  const options = {
+    disableDefaultUI: true,
+    zoomControl: true
+  }
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyBTVIJwaV2ppI-Cb6H8NMfG3ALh0NztJfQ',
+    libraries
+  })
+
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading Maps";
+
+  return (
+    <div className='PickupMap'>
+      <GoogleMap 
+        mapContainerStyle={mapContainerStyle}
+        zoom={13}
+        center={center}
+        options={options}
+      >
+      </GoogleMap>
+    </div>
+  );
 };
 
-const MapContainer = (props) => {
-  return (
-    <Map
-      google={props.google}
-      zoom={14}
-      style={mapStyles}
-      initialCenter={
-        {
-          lat: -1.2884,
-          lng: 36.8233
-        }
-      }
-    />
-  );
-}
-
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyBTVIJwaV2ppI-Cb6H8NMfG3ALh0NztJfQ'
-})(MapContainer);
+export default PickupMap;
